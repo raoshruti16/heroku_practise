89e1993f1eb64bec041f28c8112dec15fd87e29f
Shruti Rao <48805685+raoshruti16@users.noreply.github.com>
2020-06-26 23:20:15 +0530

    Update add.go

--- add.go
+++ add.go
@@ -5,6 +5,9 @@ import(
 func main(){
 fmt.Println("hello")
 fmt.Println(add(4,8))
+port := os.Getenv("PORT")
+http.HandleFunc("/",add)
+http.ListenAndServe(":"+port, nil)
 }
 func add(x int, y int) int{
 	z := 0
